c3fdd46076a6da94e6c4cac1543d6495537308dc
rmariscal <mariscal.rmc@gmail.com>
2017-05-23 18:32:49 +0200

    Creat el formulari. Agafa els valors de usuari i password

---
+++ src/Pruebas.java
@@ -0,0 +1,15 @@
+import com.sun.corba.se.impl.encoding.CodeSetConversion;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by rmariscal on 23/05/17.
+ */
+public class Pruebas {
+    public static void main(String[] args) {
+
+        }
+    }
+}
